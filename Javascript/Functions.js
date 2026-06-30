4da2aeb63bda9ea340e6aec2c0b4da6aa76b8d1f
mohinisharma-04 <mohinisharma2720@gmail.com>
2026-06-30 22:04:06 +0530

    Loops and Functions

---
+++ Javascript/Functions.js
@@ -0,0 +1,32 @@
+// function nice(name){
+//     console.log("Hey! " + name + " you are not nice!")
+//     console.log(name + "," + " you are bad")
+//     console.log(name + "," + " you are useless!")
+//     console.log(name + "," + " you don't deserve anything!!!!!!")
+//     console.log(name + "," + " you are a failure in everything!!!!")
+// };
+
+// nice("Mohini");
+// nice("Moni");
+
+// function sum(a, b, c = 5) {
+//     // console.log(a + b);
+//     return a + b + c;
+// };
+
+// result1 = sum(15, 35);
+// result2 = sum(27, 13);
+// result3 = sum(4, 26);
+
+// console.log("The sum of these numbers is " + result1);
+// console.log("The sum of these numbers is " + result2);
+// console.log("The sum of these numbers is " + result3);
+
+
+const func1 = (x) => {
+    console.log("I am an arrow function", x);
+};
+
+func1(34);
+func1(66);
+func1(84);
---
+++ Javascript/Loops.js
@@ -0,0 +1,42 @@
+console.log("I am a tutorial on Loops")
+
+let a = 1;
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name: "Mohini",
+//     role: "Programmer",
+//     company: "CodeWithHarry AI"
+// };
+
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key, element);
+//     }
+// };
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(element);
+// }
+
+// for (const c of "Mohini") {
+//     console.log(c);
+// }
+
+
+// i = 0;
+// while (i < 6) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 0;
+do {
+    console.log(i)
+    i++;
+} while (i < 6); 
