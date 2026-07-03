416894acebbf162d831944fb3bcc374405678413
mohinisharma-04 <mohinisharma2720@gmail.com>
2026-07-03 21:58:27 +0530

    DOM

---
+++ Javascript/DOM_Nodes.js
@@ -0,0 +1,19 @@
+console.log("Hello, World!");
+// console.log(document.body.childNodes);
+// console.log(document.body);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastElementChild);
+// console.log(document.body.innerHTML);
+
+// let cont = document.body;
+// console.log(cont.childNodes);
+// console.log(cont.firstChild);
+// console.log(cont.lastChild);
+
+// console.log("Hello world")
+
+// document.body.firstElementChild 
+// document.body.firstElementChild.childNodes 
+// document.body.firstElementChild.children
